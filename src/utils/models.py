2ec53b88d9cf794cd8baee9204540bca89563110
Dominik Kukla <dominik.kukla@onet.pl>
2024-12-23 16:48:18 +0100

    Added view_models.py code to Models class

--- src/utils/models.py
+++ src/utils/models.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from typing import Dict
 
 import kaggle  # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
 from torchvision import models  # type: ignore
@@ -106,3 +108,62 @@ class Models:
         self.model_weights[model_name] = checkpoint["best_val_f1"]
 
         return model.to(self.device)
+
+    def plot_training_metrics(self, checkpoint_path: str, name_of_the_net: str):
+        checkpoint = torch.load(checkpoint_path)
+        losses_val = checkpoint["val_loss"]
+        losses_train = checkpoint["train_loss"]
+        f1_metric_val = checkpoint["f1_metric_val"]
+
+        plt.figure(figsize=(10, 7))
+        marker_on = [losses_val.index(min(losses_val))]
+
+        # Create the first plot with the left y-axis
+        fig, ax1 = plt.subplots(figsize=(10, 7))
+
+        # Plot train and validation loss on the left y-axis
+        ax1.plot(
+            np.arange(1, len(losses_train) + 1),
+            losses_train,
+            color="r",
+            label="Train loss",
+        )
+        ax1.plot(
+            np.arange(1, len(losses_val) + 1),
+            losses_val,
+            "-gD",
+            label="Test loss",
+            markevery=marker_on,
+        )
+
+        # Annotate the minimum loss point
+        bbox = dict(boxstyle="round", fc="0.8")
+        ax1.annotate(
+            text=f"Max F1 score after {f1_metric_val.index(max(f1_metric_val))+1} epochs, equals: {max(f1_metric_val)}",
+            xy=(losses_val.index(min(losses_val)) + 1, min(losses_val)),
+            xytext=(losses_val.index(min(losses_val)) + 1, min(losses_val) + 0.05),
+            arrowprops=dict(facecolor="green", shrink=0.2),
+            bbox=bbox,
+        )
+
+        ax1.set_xlabel("Epoch")
+        ax1.set_ylabel("CrossEntropyLoss")
+        ax1.grid()
+
+        # Create the second y-axis for F1 metric
+        ax2 = ax1.twinx()
+        ax2.plot(
+            np.arange(1, len(f1_metric_val) + 1),
+            f1_metric_val,
+            color="blue",
+            label="F1 Metric (Val)",
+        )
+        ax2.set_ylabel("f1_metric_val")
+
+        # Combine legends
+        lines1, labels1 = ax1.get_legend_handles_labels()
+        lines2, labels2 = ax2.get_legend_handles_labels()
+        ax1.legend(lines1 + lines2, labels1 + labels2, loc="upper left")
+
+        plt.title(f"Train and Val loss, F1 Metric after {len(losses_train)} epochs")
+        plt.savefig(f"data/plots/{name_of_the_net}.png")
